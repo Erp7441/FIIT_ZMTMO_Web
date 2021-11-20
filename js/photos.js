2761c76616f08bca6fec7b400035841a909e337d
Martin Szabo <xszabom5@stuba.sk>
2021-11-20 23:13:00 +0100

    Pridanie počítadla do galérie

--- js/photos.js
+++ js/photos.js
@@ -5,10 +5,14 @@
 const photosNextBtn = document.getElementById('photosNextBtn');
 const photosPreviousBtn = document.getElementById('photosPreviousBtn');
 const photoFrame = document.getElementById('photoFrame');
+const allCounter = document.getElementById('allCounter');
+const currentCounter = document.getElementById('currentCounter');
 
 //------------------------------------------------VARIABLES------------------------------------------------
 
-let imagesIndex = 0; let img;
+let imagesIndex = 0;
+let img;
+let wait = 0;
 let images = [
     'images/fotky/back.jpg',
     'images/fotky/front.jpg',
@@ -26,56 +30,75 @@ function createImage(imagesArray, imageIndex, direction) {
     let imgObject = document.createElement("img");
     imgObject.setAttribute("src", imagesArray[imageIndex]);
     imgObject.setAttribute("class", "slideImg");
-    if(direction == "right"){
-        imgObject.style.left = "200vw";
-    }
-    else if(direction == "left"){
-        imgObject.style.left = "-200vw";
+    if (direction == "left") {
+        imgObject.style.left = "110vw";
+    } else if (direction == "right") {
+        imgObject.style.left = "-110vw";
     }
     photoFrame.appendChild(imgObject);
     setTimeout(() => {
         imgObject.style.left = "var(--photoSize)";
-    }, 500);
-    
+        currentCounter.innerHTML = imagesIndex+1;
+    }, 300);
     return imgObject;
 }
 
-function removeImage(direction){
-    if(direction == "right"){
-        img.style.left = "-200vw";
-    }
-    else if(direction == "left"){
-        img.style.left = "200vw"
-    }
-    if (img) {
+function removeImage(direction, imgObject) {
+    setTimeout(() => {
+        if (direction == "left") {
+            imgObject.style.left = "110vw";
+        } else if (direction == "right") {
+            imgObject.style.left = "-110vw"
+        }
+    }, 150);
+    if (imgObject) {
         setTimeout(() => {
             while (photoFrame.childNodes.length > 1) {
                 photoFrame.removeChild(photoFrame.firstChild);
             }
-        },3000);
+        }, 30000);
     }
 }
 
 //------------------------------------------------INITIALIZATION------------------------------------------------
 
 img = createImage(images, imagesIndex);
+allCounter.innerHTML = images.length;
 
 //------------------------------------------------SLIDESHOW------------------------------------------------
 
 photosNextBtn.addEventListener('click', () => {
-    imagesIndex++;
-    if(imagesIndex > images.length - 1){
-        imagesIndex = 0;
+    if(wait == 0) {
+        imagesIndex++;
+        if (imagesIndex > images.length - 1) {
+            imagesIndex = 0;
+        }
+        removeImage("left", img);
+        img = createImage(images, imagesIndex, "right");
+        wait = 1;
     }
-    removeImage("left");
-    img = createImage(images, imagesIndex, "left");
+    else{
+        return;
+    }
+    setTimeout(() => {
+        wait = 0;
+    }, 1000);
 });
 
 photosPreviousBtn.addEventListener('click', () => {
-    imagesIndex--;
-    if(imagesIndex <= 0){
-        imagesIndex = images.length - 1;
+    if(wait == 0) {
+        imagesIndex--;
+        if (imagesIndex < 0) {
+            imagesIndex = images.length - 1;
+        }
+        removeImage("right", img);
+        img = createImage(images, imagesIndex, "left");
+        wait = 1;
     }
-    removeImage("right");
-    img = createImage(images, imagesIndex, "right");
+    else{
+        return;
+    }
+    setTimeout(() => {
+        wait = 0;
+    }, 1000);
 });
